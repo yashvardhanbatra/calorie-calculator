dd682498076fe9c74bac74693e2778dcd327e4cc
yashvardhanbatra <yashvardhanbatra410@gmail.com>
2025-02-15 00:29:49 +1000

    new commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
+import './globals.css'
+
 export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    );
-  }
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
